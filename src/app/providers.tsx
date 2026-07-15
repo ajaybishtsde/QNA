15bde751dab139050aea7ad4b437ea12ebc0fcf5
ics-ajay <ajay.bisht@inaraconsultancy.com>
2026-07-15 12:28:49 +0530

    added shadcn and theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,17 @@
 "use client";
-import { Provider } from "react-redux";
-import { store } from "../store";
-type Props = {
-  children: React.ReactNode;
-};
 
-export default function Providers({ children }: Props) {
-  return <Provider store={store}>{children}</Provider>;
+import { Provider } from "react-redux";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "../components/providers/theme-provider";
+import { queryClient } from "../lib/react-query";
+import { store } from "../store";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>{children}</ThemeProvider>
+      </QueryClientProvider>
+    </Provider>
+  );
 }
